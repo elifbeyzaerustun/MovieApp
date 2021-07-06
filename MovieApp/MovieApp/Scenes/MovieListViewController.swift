4b9431a5c131dbd067cd9419077f0876f5ba5914
Elif Erüstün <elifbeyzaerustun@gmail.com>
2021-07-06 18:11:21 +0300

    Movie list ui implementation

---
+++ MovieApp/MovieApp/Scenes/MovieListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieListViewController.swift
+//  MovieApp
+//
+//  Created by Elif Erustun on 6.07.2021.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var collectionView: UICollectionView!
+    @IBOutlet private weak var loadMoreButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
