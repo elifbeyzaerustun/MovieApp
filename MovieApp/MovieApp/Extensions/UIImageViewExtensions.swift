a8d695e3838e3251a115932873cb981312d4612f
Elif Erüstün <elifbeyzaerustun@gmail.com>
2021-07-09 10:17:33 +0300

    Cached load image implementation

--- MovieApp/MovieApp/Extensions/UIImageViewExtensions.swift
+++ MovieApp/MovieApp/Extensions/UIImageViewExtensions.swift
@@ -9,16 +9,24 @@ import UIKit
 
 extension UIImageView {
     
-    func loadImage(with url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            guard let self = self else { return }
-            if let imageData = try? Data(contentsOf: url) {
-                if let image = UIImage(data: imageData) {
+    func loadImage(url: URL, placeholder: UIImage?, cache: URLCache? = nil) {
+        let cache = cache ?? URLCache.shared
+        let request = URLRequest(url: url)
+        if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        } else {
+            self.image = placeholder
+            URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                if let data = data, let response = response, ((response as? HTTPURLResponse)?.statusCode ?? 500) < 300, let image = UIImage(data: data) {
+                    let cachedData = CachedURLResponse(response: response, data: data)
+                    cache.storeCachedResponse(cachedData, for: request)
                     DispatchQueue.main.async {
                         self.image = image
                     }
                 }
-            }
+            }).resume()
         }
     }
 }
